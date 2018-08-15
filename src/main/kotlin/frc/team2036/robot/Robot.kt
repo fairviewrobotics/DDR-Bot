4f865938cbf2d9cbf1c9b5d2d3103b930f1ad0ea
Kyle Pfromer <kpfromer2@gmail.com>
2018-08-15 14:17:17 -0600

    Update to turn properly

--- src/main/kotlin/frc/team2036/robot/Robot.kt
+++ src/main/kotlin/frc/team2036/robot/Robot.kt
@@ -9,20 +9,26 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive
 const val XBOX_CONTROLLER_PORT = 0
 const val LEFT_TALON_PORT = 3
 const val RIGHT_TALON_PORT = 0
+//const val LEFT_BACK_TALON_PORT = 3
+//const val RIGHT_BACK_TALON_PORT = 3
 
 /**
  * The main and only class for the DDR bot
  */
 class Robot : IterativeRobot() {
 
-    lateinit var controller: Joystick
+    lateinit var controller: XboxController
     lateinit var driveTrain: DifferentialDrive
 
     /**
      * Initializer that sets up the xbox controller and the drivetrain
      */
     override fun teleopInit() {
-        this.controller = Joystick(XBOX_CONTROLLER_PORT)
+        this.controller = XboxController(XBOX_CONTROLLER_PORT)
+//        val left = WPI_TalonSRX(LEFT_TALON_PORT)
+//        val right = WPI_TalonSRX(RIGHT_TALON_PORT)
+//        left.follow(WPI_TalonSRX(LEFT_BACK_TALON_PORT))
+//        right.follow(WPI_TalonSRX(RIGHT_BACK_TALON_PORT))
         this.driveTrain = DifferentialDrive(WPI_TalonSRX(LEFT_TALON_PORT), WPI_TalonSRX(RIGHT_TALON_PORT))
     }
 
@@ -30,7 +36,7 @@ class Robot : IterativeRobot() {
      * Polls the DDR mat and then runs the robot
      */
     override fun teleopPeriodic() {
-        val limit = .5
+        val limit = .75
         var x = 0.0
         var y = 0.0
 //        for (i in 1..16) {
@@ -38,15 +44,13 @@ class Robot : IterativeRobot() {
 //                println("WORKING FOR PORT $i")
 //            }
 //        }
-        when {
-            this.controller.getRawButton(13) -> y = limit
-            this.controller.getRawButton(15) -> y = -limit
-            this.controller.getRawButton(16) -> x = -limit
-            this.controller.getRawButton(14) -> x = limit
-        }
+        if (this.controller.getRawButton(16)) y = limit
+        if (this.controller.getRawButton(14)) y = -limit
+        if (this.controller.getRawButton(13)) x = -limit
+        if (this.controller.getRawButton(15)) x = limit
 //        println(x)
 //        println(y)
-        this.driveTrain.tankDrive(x, y)
+        this.driveTrain.arcadeDrive(x, -y)
     }
 
 }
