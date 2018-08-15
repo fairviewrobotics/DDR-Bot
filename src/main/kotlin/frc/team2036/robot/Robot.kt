12b5533014063931204c18687737307ba7d320c9
Kyle Pfromer <kpfromer2@gmail.com>
2018-08-15 12:08:14 -0600

    Update for DDR

--- src/main/kotlin/frc/team2036/robot/Robot.kt
+++ src/main/kotlin/frc/team2036/robot/Robot.kt
@@ -2,10 +2,11 @@ package frc.team2036.robot
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX
 import edu.wpi.first.wpilibj.IterativeRobot
+import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.XboxController
 import edu.wpi.first.wpilibj.drive.DifferentialDrive
 
-const val XBOX_CONTROLLER_PORT = 1
+const val XBOX_CONTROLLER_PORT = 0
 const val LEFT_TALON_PORT = 3
 const val RIGHT_TALON_PORT = 0
 
@@ -14,14 +15,14 @@ const val RIGHT_TALON_PORT = 0
  */
 class Robot : IterativeRobot() {
 
-    lateinit var controller: XboxController
+    lateinit var controller: Joystick
     lateinit var driveTrain: DifferentialDrive
 
     /**
      * Initializer that sets up the xbox controller and the drivetrain
      */
     override fun teleopInit() {
-        this.controller = XboxController(XBOX_CONTROLLER_PORT)
+        this.controller = Joystick(XBOX_CONTROLLER_PORT)
         this.driveTrain = DifferentialDrive(WPI_TalonSRX(LEFT_TALON_PORT), WPI_TalonSRX(RIGHT_TALON_PORT))
     }
 
@@ -29,7 +30,23 @@ class Robot : IterativeRobot() {
      * Polls the DDR mat and then runs the robot
      */
     override fun teleopPeriodic() {
-        this.driveTrain.tankDrive(this.controller.x, this.controller.y)
+        val limit = .5
+        var x = 0.0
+        var y = 0.0
+//        for (i in 1..16) {
+//            if (this.controller.getRawButtonPressed(i)) {
+//                println("WORKING FOR PORT $i")
+//            }
+//        }
+        when {
+            this.controller.getRawButton(13) -> y = limit
+            this.controller.getRawButton(15) -> y = -limit
+            this.controller.getRawButton(16) -> x = -limit
+            this.controller.getRawButton(14) -> x = limit
+        }
+//        println(x)
+//        println(y)
+        this.driveTrain.tankDrive(x, y)
     }
 
 }
